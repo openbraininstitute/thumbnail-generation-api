577bb8b711e03da7403c3e0069b3b91e52885dc2
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-01-17 09:00:45 +0000

    Fix dotenv loading in local

--- api/api/config.py
+++ api/api/config.py
@@ -3,6 +3,12 @@ Module to setup the environment variables of the application
 """
 
 import os
+from dotenv import load_dotenv
+import matplotlib
+
+matplotlib.use("agg")
+
+load_dotenv()
 
 WHITELISTED_CORS_URLS = os.environ.get("WHITELISTED_CORS_URLS", "")
 DEBUG_MODE = os.environ.get("DEBUG_MODE", False)
