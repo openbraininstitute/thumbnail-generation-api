cd0a3edff6fc0ee21d23b6dc1084832771f7622c
Konstantinos Platis <5221058+kplatis@users.noreply.github.com>
2024-08-08 15:57:29 +0200

    Remove deprecated soma endpoint

--- api/router/swc.py
+++ api/router/swc.py
@@ -8,12 +8,11 @@ SWC file from Nexus Delta and processing it.
 """
 
 import os
-import shutil
 import subprocess
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
-from fastapi import APIRouter, Depends, File, HTTPException, Query, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, Query
 from fastapi.responses import FileResponse
 from fastapi.security import HTTPBearer
 from starlette.requests import Request
@@ -24,68 +23,6 @@ from api.services.nexus import fetch_file_content
 
 router = APIRouter()
 require_bearer = HTTPBearer()
-
-
-# MARK: Endpoints
-@router.post(
-    "/process-swc",
-    dependencies=[Depends(require_bearer)],
-)
-async def process_swc(file: UploadFile = File(...)) -> FileResponse:
-    """Process the uploaded SWC file and return the generated mesh file."""
-    temp_file_path = ""
-    try:
-        with NamedTemporaryFile(delete=False, suffix=".swc") as temp_file:
-            shutil.copyfileobj(file.file, temp_file)
-            temp_file_path = temp_file.name
-
-        logger.info("Temporary SWC file created at: %s", temp_file_path)
-
-        current_directory = Path(__file__).parent
-        output_directory = current_directory.parent.parent / "output"
-        meshes_directory = output_directory / "meshes"
-        meshes_directory.mkdir(exist_ok=True, parents=True)
-
-        script_path = current_directory.parent.parent / "neuromorphovis.py"
-        blender_executable_path = current_directory.parent.parent / "blender/bbp-blender-3.5/blender-bbp/blender"
-
-        logger.info("Running NMV script...")
-        command = [
-            "python",
-            script_path.as_posix(),
-            f"--blender={blender_executable_path.as_posix()}",
-            "--input=file",
-            f"--morphology-file={temp_file_path}",
-            "--export-soma-mesh-blend",
-            "--export-soma-mesh-obj",
-            f"--output-directory={output_directory.as_posix()}",
-        ]
-
-        subprocess.run(command, check=True)
-        logger.info("Completed NMV script execution.")
-
-        target_name = Path(temp_file_path).stem
-
-        for mesh in meshes_directory.iterdir():
-            logger.debug("Checking mesh file: %s", mesh.name)
-            if mesh.suffix == ".glb":
-                name = mesh.stem.split("_")[-1]
-                if name == target_name:
-                    logger.info("Generated mesh file found: %s", mesh.as_posix())
-                    break
-        else:
-            logger.error("OBJ file not found after processing.")
-            raise HTTPException(status_code=404, detail="OBJ file not found after processing.")
-
-        return FileResponse(
-            path=mesh,
-            media_type="model/gltf+json",
-            filename=mesh.name,
-        )
-    finally:
-        if temp_file_path and os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
-            logger.info("Temporary file deleted: %s", temp_file_path)
 
 
 @router.get(
