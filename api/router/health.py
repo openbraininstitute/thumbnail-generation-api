29556880a1b3da29267489894843685d12bc643a
Gianluca Ficarelli <gianluca.ficarelli@gmail.com>
2025-08-07 07:56:33 +0200

    Disable cache in health endpoint

--- api/router/health.py
+++ api/router/health.py
@@ -4,12 +4,14 @@ Module: health.py
 This module provides a simple health check endpoint for the web server.
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from api.dependencies import CacheControl
 
 router = APIRouter()
 
 
-@router.get("/health")
-async def health():
+@router.get("/health", dependencies=[Depends(CacheControl("no-cache"))])
+async def health() -> dict:
     """Simple health check endpoint"""
     return {"status": "OK"}
